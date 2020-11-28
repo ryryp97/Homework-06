71465968f948a23f14e4c058554c9656b233862c
ryry <ryanpetrecca@gmail.com>
2020-11-28 14:27:11 -0800

    added ajax requests to open weather api

--- script.js
+++ script.js
@@ -1,5 +1,35 @@
 $(document).ready(function() {
     console.log("ready!");
 
+    var submitBtn = $(".submitBtn");
+
+    var cityData = $("#city").text();
+
+    submitBtn.on("click", function() {
+      preventDefault();
+      console.log(cityData);
+      console.log("yoyo")
+    })
+
+    var cityWeather = function() {
+        var queryURL = "https://cors-anywhere.herokuapp.com/api.openweathermap.org/data/2.5/forecast?q=London&appid=6b9535f80b269234aa9c6fcf1d77ba62";
+        $.ajax({
+          url: queryURL,
+          method: "GET"
+        }).then(function(response) {
+          console.log(response);
+          var lat = response.city.coord.lat;
+          var lon = response.city.coord.lon;
+          var queryURL2 = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&units=imperial&appid=6b9535f80b269234aa9c6fcf1d77ba62"
+          $.ajax({
+            url: queryURL2,
+            method: "GET"
+          }).then(function(data) {
+            console.log(data);
+          })
+        });
+      };
+  
+    cityWeather();
     
 });
