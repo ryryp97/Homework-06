c4616cbf226bb765df094614ff31410dd31c4f26
ryry <ryanpetrecca@gmail.com>
2020-11-28 23:26:54 -0800

    added more data to display and added a featrue which allows the weather data from previously entered cities to be reshown

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 $(document).ready(function () {
-  console.log("ready!");
-
   var d = new Date();
-
-  console.log(d.toDateString());
-
+  //This code checks to see if there are any past cities in localStorage and will display a card if there is a city in localStorage
+  if (localStorage.getItem("city") !== null) {
+    var cityDiv = $("<div>").attr("class", "card col-6");
+    var p = $("<p>").text(localStorage.getItem("city"));
+    cityDiv.append(p);
+    $(".pastCities").prepend(cityDiv);
+  };
+  //On-click the value entered in the input field will be used to generate the weather data from that location
   $(".submitBtn").click( function() {
     event.preventDefault();
     var cityName = $(".city").val();
@@ -14,8 +17,18 @@ $(document).ready(function () {
     cityDiv.append(p);
     $(".pastCities").prepend(cityDiv);
     cityWeather(cityName);
+    localStorage.setItem("city", cityName);
+    $("p").click( function() {
+      var p = $(this).text();
+      cityWeather(p);
+    })
   });
-
+  //This allows previous weather data to be re-shown, this is alos placed within the on click function for the submit button to allow it to be applied to newly generated p elements
+  $("p").click( function() {
+    var p = $(this).text();
+    cityWeather(p);
+  })
+  //Calls the open weather api and is able
   var cityWeather = function (cityName) {
     var queryURL = "https://cors-anywhere.herokuapp.com/api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&appid=6b9535f80b269234aa9c6fcf1d77ba62";
     $.ajax({
@@ -34,11 +47,15 @@ $(document).ready(function () {
         $(".cityDate").empty();
         $(".temp").empty();
         $(".humid").empty();
+        $(".wind").empty();
+        $(".uv").empty();
         $(".weatherData").removeClass("hide");
         $(".cityDate").text(cityName + " " + d.toDateString());
         $(".cityDate").append("<img src=http://openweathermap.org/img/wn/" + data.current.weather[0].icon + "@2x.png>")
         $(".temp").text(data.current.temp);
         $(".humid").text(data.current.humidity);
+        $(".wind").text(data.current.wind_speed);
+        $(".uv").text(data.current.uvi);
       })
     });
   };
